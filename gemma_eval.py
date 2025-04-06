a1e51e4f49c99d7fdb164deb27f3ee3158143570
axeld5 <axel.darmouni@student-cs.fr>
2025-04-06 21:27:08 +0200

    added eval and sft

---
+++ gemma_eval.py
@@ -0,0 +1,38 @@
+from rewards import reward_similarity, reward_haiku
+from unsloth import FastModel
+import tqdm
+from datasets import load_dataset
+
+def eval_model(model_path, prompts):
+    model, tokenizer = FastModel.from_pretrained(
+        model_name = model_path,
+        max_seq_length = 2048,
+        load_in_4bit = True,
+    )
+    n = len(prompts)
+    haiku_val = 0
+    similarity_val = 0
+    val = 0
+    for prompt in tqdm.tqdm(prompts):
+        messages = [prompt]
+        text = tokenizer.apply_chat_template(
+            messages,
+            tokenize = False,
+            add_generation_prompt = True, # Must add for generation
+        )
+        outputs = model.generate(
+            **tokenizer([text], return_tensors = "pt").to("cuda"),
+            max_new_tokens = 50,
+            temperature = 1.0, top_p = 0.95, top_k = 64,
+        )
+        generated_text = tokenizer.batch_decode(outputs)[0].split("model\n")[1].split("\n<end_of_turn>")[0].strip()
+        assignment = prompt["content"].split(":")[1].strip()
+        haiku_val += reward_haiku(generated_text)/n
+        similarity_val += (reward_similarity(assignment, generated_text)/n).item()
+        val = (haiku_val+similarity_val)
+    return {"is_haiku_avg":haiku_val, "similarity_avg":similarity_val, "total_avg": val}
+
+if __name__ == "__main__":
+    dataset = load_dataset("json", data_files="eval_dataset/eval_data.json")["train"]
+    prompts = [elem["conversations"][0] for elem in dataset]
+    eval_model("unsloth/gemma-3-1b-it", prompts)
---
+++ gemma_sft.py
@@ -0,0 +1,74 @@
+from unsloth import FastModel
+from unsloth.chat_templates import get_chat_template
+from unsloth.chat_templates import standardize_data_formats
+from datasets import load_dataset
+from trl import SFTTrainer, SFTConfig
+from unsloth.chat_templates import train_on_responses_only
+
+model, tokenizer = FastModel.from_pretrained(
+    model_name = "unsloth/gemma-3-1b-it",
+    max_seq_length = 2048, # Choose any for long context!
+    load_in_4bit = True,  # 4 bit quantization to reduce memory
+    load_in_8bit = False, # [NEW!] A bit more accurate, uses 2x memory
+    full_finetuning = False, # [NEW!] We have full finetuning now!
+)
+
+model = FastModel.get_peft_model(
+    model,
+    finetune_vision_layers     = False, # Turn off for just text!
+    finetune_language_layers   = True,  # Should leave on!
+    finetune_attention_modules = True,  # Attention good for GRPO
+    finetune_mlp_modules       = True,  # SHould leave on always!
+    r = 8,           # Larger = higher accuracy, but might overfit
+    lora_alpha = 8,  # Recommended alpha == r at least
+    lora_dropout = 0,
+    bias = "none",
+    random_state = 3407,
+)
+
+tokenizer = get_chat_template(
+    tokenizer,
+    chat_template = "gemma-3",
+)
+
+data = load_dataset('json', data_files="train_dataset/sft_data.json")["train"]
+dataset = standardize_data_formats(data)
+
+def apply_chat_template(examples):
+    texts = tokenizer.apply_chat_template(examples["conversations"])
+    return { "text" : texts }
+
+dataset = dataset.map(apply_chat_template, batched = True)
+
+trainer = SFTTrainer(
+    model = model,
+    tokenizer = tokenizer,
+    train_dataset = dataset,
+    eval_dataset = None, # Can set up evaluation!
+    args = SFTConfig(
+        dataset_text_field = "text",
+        per_device_train_batch_size = 2,
+        gradient_accumulation_steps = 4, # Use GA to mimic batch size!
+        warmup_steps = 5,
+        num_train_epochs = 1, # Set this for 1 full training run.
+        max_steps = 30,
+        learning_rate = 2e-4, # Reduce to 2e-5 for long training runs
+        logging_steps = 1,
+        optim = "adamw_8bit",
+        weight_decay = 0.01,
+        lr_scheduler_type = "linear",
+        seed = 3407,
+        report_to = "none", # Use this for WandB etc
+    ),
+)
+
+trainer = train_on_responses_only(
+    trainer,
+    instruction_part = "<start_of_turn>user\n",
+    response_part = "<start_of_turn>model\n",
+)
+
+trainer_stats = trainer.train()
+
+model.save_pretrained("gemma-3-haiku")  # Local saving
+tokenizer.save_pretrained("gemma-3-haiku")
