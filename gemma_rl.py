39d1c5df1fc6c2e312ce90770f30c193f9b50042
axeld5 <axel.darmouni@student-cs.fr>
2025-04-07 22:20:42 +0200

    modified args

--- gemma_rl.py
+++ gemma_rl.py
@@ -44,25 +44,22 @@ model.print_trainable_parameters()
 max_prompt_length = 256
 
 training_args = GRPOConfig(
-    learning_rate = 5e-6,
-    adam_beta1 = 0.9,
-    adam_beta2 = 0.99,
-    weight_decay = 0.1,
-    warmup_ratio = 0.1,
-    lr_scheduler_type = "cosine",
-    optim = "adamw_torch_fused",
-    logging_steps = 1,
-    per_device_train_batch_size = 1,
-    gradient_accumulation_steps = 1, # Increase to 4 for smoother training
-    num_generations = 4, # Decrease if out of memory
-    max_prompt_length = max_prompt_length,
-    max_completion_length = max_seq_length - max_prompt_length,
-    num_train_epochs = 1, # Set to 1 for a full training run
-    max_steps = 50,
-    save_steps = 50,
-    max_grad_norm = 0.1,
-    report_to = "none", # Can use Weights & Biases
-    output_dir = "outputs",
+    output_dir="gemma-3-1b-rl",
+    learning_rate=1e-5,
+    remove_unused_columns=False,  # to access the solution column in accuracy_reward
+    gradient_accumulation_steps=16,
+    num_train_epochs=1,
+    bf16=True,
+    # Parameters that control de data preprocessing
+    max_completion_length=64,  # default: 256
+    num_generations=4,  # default: 8
+    max_prompt_length=128,  # default: 512
+    # Parameters related to reporting and saving
+    report_to=None,
+    logging_steps=10,
+    push_to_hub=False,
+    save_strategy="steps",
+    save_steps=10,
 )
 
 def reward_bad():
@@ -71,7 +68,7 @@ def reward_bad():
 trainer = GRPOTrainer(
     model = model,
     reward_funcs = [
-        reward_bad
+        compute_train_rewards
     ],
     args = training_args,
     train_dataset = dataset,
