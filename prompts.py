9083be0a3359825f5f7bae814d29e588ffc59f1a
axeld5 <axel.darmouni@student-cs.fr>
2025-04-13 23:35:13 +0200

    adding hub push function

--- prompts.py
+++
@@ -1,5 +0,0 @@
-def summarize_haiku_prompt(haiku:str) -> str:
-    prompt = f"""You are a model tasked to sum up very shortly what the haiku is about.
-    <haiku> {haiku} </haiku>
-    Output the summary and only the summary:"""
-    return prompt
